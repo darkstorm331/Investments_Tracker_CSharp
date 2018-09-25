92c7f4491763c203752cd4d72b3a53e418f1b688
Michael Brand <michaelbrand@ntlworld.com>
2018-09-25 17:18:01 +0100

    25th September 2018

---
+++ Investments_Tracker/LendyLoginPage.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Investments_Tracker
+{
+    /// <summary>
+    /// Interaction logic for LendyLoginPage.xaml
+    /// </summary>
+    public partial class LendyLoginPage : Window
+    {
+        private static readonly HttpClient client = new HttpClient();
+        public string lendyResponse { get; set; }
+
+        public LendyLoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+
+        private void Confirm_Click(object sender, RoutedEventArgs e)
+        {
+            string emailAdd = txt_EmailAddress.Text;
+            string password = txt_Password.Password;
+
+            var values = new Dictionary<string, string>
+            {
+                {"email", emailAdd},
+                {"password", password}
+            };
+
+            var content = new FormUrlEncodedContent(values);
+
+            var postTask = Task.Run(async () => {
+                var response = await client.PostAsync("https://lendy.co.uk/login", content);
+
+                var responseString = await response.Content.ReadAsStringAsync();
+
+                lendyResponse = responseString;
+            });
+
+            postTask.Wait();
+            Console.WriteLine(lendyResponse);
+
+            if (lendyResponse.Contains("<p class='c-flash-message__message'>Couldn't login with those details</p>"))
+            {
+                MessageBox.Show("The given username or password was incorrect", "Invalid Credentials", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+            }
+            else
+            {               
+                DialogResult = true;                
+                Close();
+            }
+        }
+
+        private static async Task<string> HttpPost(string emailAddress, string password)
+        {
+            var values = new Dictionary<string, string>
+            {
+                {"email", emailAddress},
+                {"password", password}
+            };
+
+            var content = new FormUrlEncodedContent(values);
+
+            var response = await client.PostAsync("https://lendy.co.uk/login", content);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            return responseString;
+        }
+
+    }
+}
--- Investments_Tracker/MainWindow.xaml.cs
+++ Investments_Tracker/MainWindow.xaml.cs
@@ -16,13 +16,314 @@ using System.Windows.Shapes;
 namespace Investments_Tracker
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Created by Michael Brand 25/09/2018
+    /// 
+    /// This code handles the logic behind the main page that the user first sees
     /// </summary>
     public partial class MainWindow : Window
     {
+        int selectedPlatform = 0;
+        List<int> currentSelections = new List<int>();
+        string postResponse;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btn_FirstConnect_Click(object sender, RoutedEventArgs e)
+        {           
+            selectedPlatform = cmb_FirstSelection.SelectedIndex;
+            System.Console.WriteLine(selectedPlatform); //For dev purposes
+            
+            //If the selected option is '*Select Platform*' then display an error message to the user
+            if(selectedPlatform == 0)
+            {
+                MessageBox.Show("Please select a platform", "Invalid Selection", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+            } else
+            {
+                //If the selected platform has already been connected to in another row then display an error message
+                if (currentSelections.Contains(selectedPlatform))
+                {
+                    MessageBox.Show("You have already connected to this platform in another row", "Already connected", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+                }
+                else
+                {
+                    bool? result = false;
+                    //Bring up the login box for the selected platform
+                    switch (selectedPlatform)
+                    {
+                        case 1:
+                            LendyLoginPage llp = new LendyLoginPage();
+                            result = llp.ShowDialog();
+                            Console.WriteLine(result); //For dev purposes 
+                            
+                            if(result == true)
+                            {
+                                postResponse = llp.lendyResponse;
+                                                            
+                            } else
+                            {
+
+                            }
+
+                            break;
+
+                        case 2:
+                            break;
+
+                        case 3:
+                            break;
+
+                        case 4:
+                            break;
+
+                        case 5:
+                            break;
+
+                        case 6:
+                            break;
+
+                        default:
+                            System.Console.WriteLine("Index not found");
+                            break;
+                    }
+
+                    if(result == true)
+                    {
+                        //Add the current selection to the list
+                        currentSelections.Add(selectedPlatform);
+                        cmb_FirstSelection.IsEnabled = false;
+                        btn_FirstConnect.IsEnabled = false;
+                    }
+                }             
+            }
+
+        }
+
+        private void btn_SecondConnect_Click(object sender, RoutedEventArgs e)
+        {
+            selectedPlatform = cmb_SecondSelection.SelectedIndex;
+
+            //If the selected option is '*Select Platform*' then display an error message to the user
+            if (selectedPlatform == 0)
+            {
+                MessageBox.Show("Please select a platform", "Invalid Selection", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+            }
+            else
+            {
+                //If the selected platform has already been connected to in another row then display an error message
+                if (currentSelections.Contains(selectedPlatform))
+                {
+                    MessageBox.Show("You have already connected to this platform in another row", "Already connected", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+                }
+                else
+                {
+                    //Add the current selection to the list
+                    currentSelections.Add(selectedPlatform);
+                    cmb_SecondSelection.IsEnabled = false;
+                }
+            }
+        }
+
+        private void btn_ThirdConnect_Click(object sender, RoutedEventArgs e)
+        {
+            selectedPlatform = cmb_ThirdSelection.SelectedIndex;
+
+            //If the selected option is '*Select Platform*' then display an error message to the user
+            if (selectedPlatform == 0)
+            {
+                MessageBox.Show("Please select a platform", "Invalid Selection", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+            }
+            else
+            {
+                //If the selected platform has already been connected to in another row then display an error message
+                if (currentSelections.Contains(selectedPlatform))
+                {
+                    MessageBox.Show("You have already connected to this platform in another row", "Already connected", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+                }
+                else
+                {
+                    //Add the current selection to the list
+                    currentSelections.Add(selectedPlatform);
+                    cmb_ThirdSelection.IsEnabled = false;
+                }
+            }
+        }
+
+        private void btn_FourthConnect_Click(object sender, RoutedEventArgs e)
+        {
+            selectedPlatform = cmb_FourthSelection.SelectedIndex;
+
+            //If the selected option is '*Select Platform*' then display an error message to the user
+            if (selectedPlatform == 0)
+            {
+                MessageBox.Show("Please select a platform", "Invalid Selection", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+            }
+            else
+            {
+                //If the selected platform has already been connected to in another row then display an error message
+                if (currentSelections.Contains(selectedPlatform))
+                {
+                    MessageBox.Show("You have already connected to this platform in another row", "Already connected", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+                }
+                else
+                {
+                    //Add the current selection to the list
+                    currentSelections.Add(selectedPlatform);
+                    cmb_FourthSelection.IsEnabled = false;
+                }
+            }
+        }
+
+        private void btn_FifthConnect_Click(object sender, RoutedEventArgs e)
+        {
+            selectedPlatform = cmb_FifthSelection.SelectedIndex;
+
+            //If the selected option is '*Select Platform*' then display an error message to the user
+            if (selectedPlatform == 0)
+            {
+                MessageBox.Show("Please select a platform", "Invalid Selection", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+            }
+            else
+            {
+                //If the selected platform has already been connected to in another row then display an error message
+                if (currentSelections.Contains(selectedPlatform))
+                {
+                    MessageBox.Show("You have already connected to this platform in another row", "Already connected", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+                }
+                else
+                {
+                    //Add the current selection to the list
+                    currentSelections.Add(selectedPlatform);
+                    cmb_FifthSelection.IsEnabled = false;
+                }
+            }
+        }
+
+        private void btn_SixthConnect_Click(object sender, RoutedEventArgs e)
+        {
+            selectedPlatform = cmb_SixthSelection.SelectedIndex;
+
+            //If the selected option is '*Select Platform*' then display an error message to the user
+            if (selectedPlatform == 0)
+            {
+                MessageBox.Show("Please select a platform", "Invalid Selection", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+            }
+            else
+            {
+                //If the selected platform has already been connected to in another row then display an error message
+                if (currentSelections.Contains(selectedPlatform))
+                {
+                    MessageBox.Show("You have already connected to this platform in another row", "Already connected", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+                }
+                else
+                {
+                    //Add the current selection to the list
+                    currentSelections.Add(selectedPlatform);
+                    cmb_SixthSelection.IsEnabled = false;
+                }
+            }
+        }
+
+        private void btn_SeventhConnect_Click(object sender, RoutedEventArgs e)
+        {
+            selectedPlatform = cmb_SeventhSelection.SelectedIndex;
+
+            //If the selected option is '*Select Platform*' then display an error message to the user
+            if (selectedPlatform == 0)
+            {
+                MessageBox.Show("Please select a platform", "Invalid Selection", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+            }
+            else
+            {
+                //If the selected platform has already been connected to in another row then display an error message
+                if (currentSelections.Contains(selectedPlatform))
+                {
+                    MessageBox.Show("You have already connected to this platform in another row", "Already connected", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+                }
+                else
+                {
+                    //Add the current selection to the list
+                    currentSelections.Add(selectedPlatform);
+                    cmb_SeventhSelection.IsEnabled = false;
+                }
+            }
+        }
+
+        private void btn_EighthConnect_Click(object sender, RoutedEventArgs e)
+        {
+            selectedPlatform = cmb_EighthSelection.SelectedIndex;
+
+            //If the selected option is '*Select Platform*' then display an error message to the user
+            if (selectedPlatform == 0)
+            {
+                MessageBox.Show("Please select a platform", "Invalid Selection", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+            }
+            else
+            {
+                //If the selected platform has already been connected to in another row then display an error message
+                if (currentSelections.Contains(selectedPlatform))
+                {
+                    MessageBox.Show("You have already connected to this platform in another row", "Already connected", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+                }
+                else
+                {
+                    //Add the current selection to the list
+                    currentSelections.Add(selectedPlatform);
+                    cmb_EighthSelection.IsEnabled = false;
+                }
+            }
+        }
+
+        private void btn_NinthConnect_Click(object sender, RoutedEventArgs e)
+        {
+            selectedPlatform = cmb_NinthSelection.SelectedIndex;
+
+            //If the selected option is '*Select Platform*' then display an error message to the user
+            if (selectedPlatform == 0)
+            {
+                MessageBox.Show("Please select a platform", "Invalid Selection", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+            }
+            else
+            {
+                //If the selected platform has already been connected to in another row then display an error message
+                if (currentSelections.Contains(selectedPlatform))
+                {
+                    MessageBox.Show("You have already connected to this platform in another row", "Already connected", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+                }
+                else
+                {
+                    //Add the current selection to the list
+                    currentSelections.Add(selectedPlatform);
+                    cmb_NinthSelection.IsEnabled = false;
+                }
+            }
+        }
+
+        private void btn_TenthConnect_Click(object sender, RoutedEventArgs e)
+        {
+            selectedPlatform = cmb_TenthSelection.SelectedIndex;
+
+            //If the selected option is '*Select Platform*' then display an error message to the user
+            if (selectedPlatform == 0)
+            {
+                MessageBox.Show("Please select a platform", "Invalid Selection", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+            }
+            else
+            {
+                //If the selected platform has already been connected to in another row then display an error message
+                if (currentSelections.Contains(selectedPlatform))
+                {
+                    MessageBox.Show("You have already connected to this platform in another row", "Already connected", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+                }
+                else
+                {
+                    //Add the current selection to the list
+                    currentSelections.Add(selectedPlatform);
+                    cmb_TenthSelection.IsEnabled = false;
+                }
+            }
         }
     }
 }
